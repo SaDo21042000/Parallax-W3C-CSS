414996c4c1e177b9f246717e19f59174f69661b1
SaDo21042000 <dovansa21042000@gmail.com>
2022-09-20 10:05:04 +0700

    do a half for about

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,11 @@
-console.log("text")
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "nav"  + " animate-top" + " nav-white" + " nav-box-shadow";
+        console.log("before")
+    } else {
+        navbar.className = navbar.className.replace("nav-white nav-box-shadow", "");
+        console.log("after")
+    }
+}
